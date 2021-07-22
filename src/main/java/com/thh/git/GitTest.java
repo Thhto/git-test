68b378fbde3686bd671efdc03b687dcdbe4cc39f
thh0331 <997058689@qq.com>
2021-07-22 16:58:01 +0800

    first commit

---
+++ src/main/java/com/thh/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.thh.git;
+
+/**
+ * @author thh
+ * @create 2021-07-22 16:47
+ */
+public class GitTest  {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
